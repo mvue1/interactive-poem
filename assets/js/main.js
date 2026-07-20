ae3199c772b360086cb70edf6c78976cd5555e56
mvue1 <dal180482@utdallas.edu>
2026-07-20 16:55:29 +0000

    added js div style transform

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,5 +18,13 @@ function randomize() {
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
+
+        // 50% of the time...
+        if (Math.random() > .5) {
+            scale = 1; // don't affect scale
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
+        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
     })
 }
