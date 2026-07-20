326307423ce73892647b02c406b677868dcf797a
mvue1 <dal180482@utdallas.edu>
2026-07-20 16:41:24 +0000

    added javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 // main.js
 
-console.log("Hello, World!");
+// Define Random Number Function
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Define divs variable as all divs inside grid container
+let divs = document.querySelectorAll(".grid-container div");
