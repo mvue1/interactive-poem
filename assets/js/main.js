a03e07d198aad6c001443f348857150ba66c2675
mvue1 <dal180482@utdallas.edu>
2026-07-20 16:47:57 +0000

    added randomize function js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,16 @@ function randomNumber(min, max) {
 
 // Define divs variable as all divs inside grid container
 let divs = document.querySelectorAll(".grid-container div");
+
+// Define Randomize Function
+function randomize() {
+    console.log(divs.length, "randomize!");
+
+    // just the grid divs
+    divs.forEach(function (div){
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+    })
+}
